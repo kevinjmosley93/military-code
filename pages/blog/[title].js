f086ae971a9fee6c8fd355508144927823ecaabf
kevinjmosley93 <kevinjmosley93@gmail.com>
2021-07-17 06:30:32 -0500

    commit

--- pages/blog/[title].js
+++ pages/blog/[title].js
@@ -1,33 +1,33 @@
 import React from 'react'
 
-const Title = ({ data }) => {
-  console.log({ blog: data })
+const Title = () => {
+  // console.log({ blog: data })
   return <div>blog id page</div>
 }
 
-export async function getStaticPaths() {
-  // Call an external API endpoint to get posts
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/get-blog`)
-  const data = await res.json()
+// export async function getStaticPaths() {
+//   // Call an external API endpoint to get posts
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/get-blog`)
+//   const data = await res.json()
 
-  console.log({ data })
+//   console.log({ data })
 
-  const paths = data.map(({ fields: { title } }) => ({
-    params: { title }
-  }))
+//   const paths = data.map(({ fields: { title } }) => ({
+//     params: { title }
+//   }))
 
-  return { paths, fallback: false }
-}
+//   return { paths, fallback: false }
+// }
 
-// This also gets called at build time
-export async function getStaticProps({ params }) {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/get-blog`)
-  const data = await res.json()
-  if (!data) return
+// // This also gets called at build time
+// export async function getStaticProps({ params }) {
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/get-blog`)
+//   const data = await res.json()
+//   if (!data) return
 
-  console.log({ data })
+//   console.log({ data })
 
-  return { props: { data } }
-}
+//   return { props: { data } }
+// }
 
 export default Title
