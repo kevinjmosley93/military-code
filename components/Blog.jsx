a4827e931b3a9230eb01d183037717ebf9ef96a9
kevinjmosley93 <kevinjmosley93@gmail.com>
2021-07-17 20:17:31 -0500

    commit

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -1,21 +1,24 @@
 import moment from 'moment'
 import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Blog = () => {
   const [blogData, setData] = useState([])
   const fetchData = async () => {
     const res = await fetch(`${window.location.origin}/api/get-blog`)
     const data = await res.json()
-    console.log('this is data', data)
+    // console.log('this is data', data)
     setData(data)
   }
+
   useEffect(() => {
     fetchData()
   }, [])
 
   const blog = blogData.map(
-    ({ fields: { title, body, author, createdAt, img, blogImg } }) => ({
+    ({ id, fields: { title, body, author, createdAt, img, blogImg } }) => ({
+      id,
       title,
       body,
       createdAt,
@@ -28,57 +31,70 @@ const Blog = () => {
   return (
     <div className='container'>
       <h1 className='text-center mt-3'>Recent Posts</h1>
-      <div className='row my-5 gx-5'>
+      <div className='row my-5 gx-5 w-100 mx-auto'>
         {blog &&
-          blog.map(({ title, body, createdAt, author, img, blogImg }, idx) => (
-            <div key={idx} className='col-lg-4 mb-5'>
-              <div className='card h-100 shadow border-0'>
-                <Image
-                  width={150}
-                  height={200}
-                  src={
-                    blogImg ||
-                    'https://dummyimage.com/150x200/000000/dbd2db.png&text=Placeholder'
-                  }
-                  alt='cover img'
-                />
-                <div className='card-body p-4'>
-                  <a className='text-decoration-none link-dark stretched-link'>
-                    <h5 className='card-title mb-3'>{title}</h5>
-                  </a>
-                  <p className='card-text text-truncate mb-0'>{body}</p>
-                </div>
-                <div className='card-footer p-4 pt-0 bg-transparent border-top-0'>
-                  <div className='d-flex align-items-end justify-content-between'>
-                    <div className='d-flex align-items-center'>
-                      <Image
-                        width={40}
-                        height={40}
-                        className='rounded-circle'
-                        src={
-                          img ||
-                          'https://dummyimage.com/40x40/000000/dbd2db.png&text=Placeholder'
-                        }
-                        alt='Author Img'
-                      />
-                      <div className='small mx-2'>
-                        <div className='fw-bold'>{author}</div>
-                        <div className='text-muted'>
-                          {moment(createdAt).format('MMMM Do YYYY')}
+          blog.map(
+            ({ id, title, body, createdAt, author, img, blogImg }, idx) => (
+              <div key={idx} className='col-lg-4 mb-5'>
+                <div className='card h-100 shadow border-0'>
+                  <Image
+                    className='rounded'
+                    width={150}
+                    height={200}
+                    src={
+                      blogImg ||
+                      'https://dummyimage.com/150x200/000000/dbd2db.png&text=Placeholder'
+                    }
+                    alt='cover img'
+                  />
+                  <div className='card-body p-4 d-flex flex-column justify-content-between'>
+                    <Link href={`/blog/${title}`}>
+                      <a className='text-decoration-none link-dark stretched-link'>
+                        <h4 className='card-title mb-3 text-truncate'>
+                          {title}
+                        </h4>
+                      </a>
+                    </Link>
+                    <span className='text-truncate-container'>
+                      <p
+                        style={{ fontSize: '.6rem' }}
+                        className='card-text m-0'>
+                        {body}
+                      </p>
+                    </span>
+                  </div>
+                  <div className='card-footer p-4 pt-0 bg-transparent border-top-0'>
+                    <div className='d-flex align-items-end justify-content-between'>
+                      <div className='d-flex align-items-center'>
+                        <Image
+                          width={40}
+                          height={40}
+                          className='rounded-circle'
+                          src={
+                            img ||
+                            'https://dummyimage.com/40x40/000000/dbd2db.png&text=Placeholder'
+                          }
+                          alt='Author Img'
+                        />
+                        <div className='small mx-2'>
+                          <div className='fw-bold'>{author}</div>
+                          <div className='text-muted'>
+                            {moment(createdAt).format('MMMM Do YYYY')}
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
-        <a
+            )
+          )}
+        {/* <a
           style={{ margin: '0 auto' }}
           href='#'
           className='text-center text-dark d-block btn-lg p-2'>
           View More
-        </a>
+        </a> */}
       </div>
     </div>
   )
--- pages/blog/[title].js
+++ pages/blog/[title].js
@@ -1,33 +1,78 @@
 import React from 'react'
+import Image from 'next/image'
+import moment from 'moment'
 
-const Title = () => {
+const Title = ({ data }) => {
+  const {
+    id,
+    fields: { title, body, author, createdAt, img }
+  } = data
+
   // console.log({ blog: data })
-  return <div>blog id page</div>
+  return (
+    <div
+      style={{
+        wordWrap: 'break-word'
+      }}
+      className='container my-3'>
+      <h1 className='text-center'>{title}</h1>
+      <span className='row'>
+        <div className='d-flex align-items-center my-4'>
+          <Image
+            width={40}
+            height={40}
+            className='rounded-circle'
+            src={
+              img ||
+              'https://dummyimage.com/40x40/000000/dbd2db.png&text=Placeholder'
+            }
+            alt='Author Img'
+          />
+          <div className='small mx-2'>
+            <div className='fw-bold'>{author}</div>
+            <div className='text-muted'>
+              {moment(createdAt).format('MMMM Do YYYY')}
+            </div>
+          </div>
+        </div>
+      </span>
+      <p className='text-justify mt-3'>{body}</p>
+    </div>
+  )
 }
 
-// export async function getStaticPaths() {
-//   // Call an external API endpoint to get posts
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/get-blog`)
-//   const data = await res.json()
+export async function getStaticPaths() {
+  const url = 'http://localhost:3000'
+  // Call an external API endpoint to get posts
+  const res = await fetch(`${url}/api/get-blog`)
+  const data = await res.json()
 
-//   console.log({ data })
+  console.log({ data })
 
-//   const paths = data.map(({ fields: { title } }) => ({
-//     params: { title }
-//   }))
+  const paths = data.map(({ id, fields: { title } }) => ({
+    params: { title }
+  }))
 
-//   return { paths, fallback: false }
-// }
+  return { paths, fallback: false }
+}
 
-// // This also gets called at build time
-// export async function getStaticProps({ params }) {
-//   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/get-blog`)
-//   const data = await res.json()
-//   if (!data) return
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+  console.log({ params })
+  const url = 'http://localhost:3000'
 
-//   console.log({ data })
+  const res = await fetch(`${url}/api/get-blog`)
+  const data = await res.json()
 
-//   return { props: { data } }
-// }
+  const newData = data.find(({ fields: { title } }) => title == params.title)
+
+  console.log({ newData })
+
+  if (!newData) return
+
+  console.log({ postData: newData })
+
+  return { props: { data: newData } }
+}
 
 export default Title
