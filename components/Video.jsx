a119fc511934f6b4a53309a22f5c74ed24e4cb56
kevinjmosley93 <kevinjmosley93@gmail.com>
2021-07-24 19:16:04 -0500

    spotify player

--- components/Video.jsx
+++ components/Video.jsx
@@ -4,12 +4,16 @@ const Video = () => {
   return (
     <div className='container'>
       <h2 className='pb-2 text-center'>What V.E.T.S are saying</h2>
-      <p className='pb-2 text-center'>
-        <audio autoPlay={true} controls>
-          <source src='/etv-podcast.mp3' type='audio/mpeg' />
-          Your browser does not support the audio element.
-        </audio>
-      </p>
+      <div className='pb-4'>
+        <iframe
+          src='https://open.spotify.com/embed/show/7DdnDy9U86NqPwN5wU6G19?theme=0'
+          width='100%'
+          height='152'
+          frameBorder='0'
+          allowTransparency='true'
+          allow='encrypted-media'
+        />
+      </div>
       <div className='row'>
         <div className='col-md-4'>
           <iframe
