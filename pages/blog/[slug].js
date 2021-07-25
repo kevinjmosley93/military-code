81383f9015e9728e96d22a207654188528f32b6b
kevinjmosley93 <kevinjmosley93@gmail.com>
2021-07-25 01:17:59 -0500

    add training to profile

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -94,7 +94,7 @@ export async function getStaticPaths() {
     params: { slug }
   }))
 
-  return { paths, fallback: false }
+  return { paths, fallback: true }
 }
 
 export async function getStaticProps({ params }) {
