843b98fdc96f3f824d46e700df245d95a04371e0
Kevin J Mosley <Kevinjmosley93@gmail.com>
2022-01-17 15:11:03 -0800

    add

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,7 +1,7 @@
 export default {
   title: 'ETV | Employ The Vets (V.E.T.S - Veterans Equally Trying to Survive)',
   description:
-    '#1 Job board for separating veterans & active duty military personnel to find available jobs and resources to find employment before their separation date.',
+    '#1 website for Veterans Looking For Jobs, Civilian Jobs For Veterans, Government Jobs After Military for Veterans, High Paying Jobs For Veterans, Jobs After Leaving The Military, Best Jobs After The Military, Remote Jobs For Veterans, Work From Home Jobs For Veterans and much more.',
   keywords: 'Civilian Jobs For Veterans, Veterans Looking For Jobs, Government Jobs After Military, High Paying Jobs For Veterans, Government Jobs For Veterans, Jobs After Leaving The Military, Best Jobs After The Military, Remote Jobs For Veterans, Work From Home Jobs For Veterans' ,
   canonical: 'https://www.employthevets.com',
   openGraph: {
@@ -10,7 +10,7 @@ export default {
     url: 'https://www.employthevets.com',
     title: 'ETV | Employ The Vets (V.E.T.S - Veterans Equally Trying to Survive)',
     description:
-      '#1 Job board for separating veterans & active duty military personnel to find available jobs and resources to find employment before their separation date.',
+      '#1 website for Veterans Looking For Jobs, Civilian Jobs For Veterans, Government Jobs After Military for Veterans, High Paying Jobs For Veterans, Jobs After Leaving The Military, Best Jobs After The Military, Remote Jobs For Veterans, Work From Home Jobs For Veterans and much more.',
     site_name: 'ETV | Employ The Vets (V.E.T.S - Veterans Equally Trying to Survive)'
   },
   twitter: {
